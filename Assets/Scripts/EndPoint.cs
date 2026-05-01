3bfad3097c431d68305e4a9f4f23e27838d5734e
novadesu <kimkup53@gmail.com>
2026-05-01 17:01:19 +0700

    main-menu-and-set-endpoint

--- Assets/Scripts/EndPoint.cs
+++ Assets/Scripts/EndPoint.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndPoint : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // เปลี่ยนจาก "Bullet" เป็น "Player"
+        // เช็กว่าสิ่งที่เดินเข้ามาชน มี Tag เป็น "Player" ใช่หรือไม่
         if (collision.CompareTag("Player"))
         {
-            Debug.Log("You Win!");
+            // หาว่าตอนนี้อยู่ด่านเลขอะไร แล้วบวก 1 เพื่อไปด่านถัดไป
+            int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+
+            // สั่งโหลดด่าน
+            SceneManager.LoadScene(nextSceneIndex);
         }
     }
 }
